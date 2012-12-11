1ddd5c319947e40af400e626531de83c74f77315
Marcus Ortiz <mportiz08@gmail.com>
2012-12-11 14:43:22 -0800

    Added a test for multiple clients.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,2 +1,18 @@
+require 'net/http'
+
 require 'rspec'
 require 'sirv'
+
+module Helpers
+  def test_port
+    3000
+  end
+  
+  def localhost
+    URI "http://localhost:#{test_port}"
+  end
+  
+  def basic_rack_app
+    ->(env) { [200, {'Content-Type' => 'text/plain'}, ['foobar']] }
+  end
+end
--- spec/server/rack_server_spec.rb
+++ spec/server/rack_server_spec.rb
@@ -2,16 +2,29 @@ require 'helper'
 require 'net/http'
 
 describe Sirv::RackServer do
+  include Helpers
+  
   before do
-    @server = Sirv::RackServer.new(3000)
+    @server = Sirv::RackServer.new(test_port)
   end
   
   it 'should serve a basic rack application' do
-    app = ->(env) { [200, {'Content-Type' => 'text/plain'}, ['hello, world!']] }
     begin
-      sthread = Thread.new { @server.run(:application => app) }
-      Net::HTTP.get(URI('http://localhost:3000')).should eq('hello, world!')
+      sthread = Thread.new { @server.run(:application => basic_rack_app) }
+      Net::HTTP.get(localhost).should eq('foobar')
     ensure
+      sthread.terminate
+    end
+  end
+  
+  it 'should handle multiple client connections' do
+    begin
+      sthread = Thread.new { @server.run(:application => basic_rack_app) }
+      clients = 5.times.map do
+        Thread.new { Net::HTTP.get(localhost).should eq('foobar') }
+      end
+    ensure
+      clients.each(&:join)
       sthread.terminate
     end
   end
